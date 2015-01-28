07fc07cf51d13cc22019aff0ce3dec1fc11140b9
Christopher Su <christophersu9@gmail.com>
2015-01-28 15:49:39 -0800

    examples

--- README.md
+++ README.md
@@ -12,7 +12,7 @@ pip install montecarlo
 from random import randint
 from montecarlo import montecarlo
 
-def flip_coin(g):
+def flip_is_heads(g):
     return randint(0, 1) == 0
 
 mc = montecarlo(flip_coin)
--- examples/coin_flip.py
+++ examples/coin_flip.py
@@ -1,9 +1,9 @@
 from random import randint
 from montecarlo import montecarlo
 
-def flip_coin(g):
+def flip_is_heads(g):
     return randint(0, 1) == 0
 
-mc = montecarlo(flip_coin)
+mc = montecarlo(flip_is_heads)
 result = mc.run()
 print "Result returned: " + str(result)
