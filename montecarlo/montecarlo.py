6a48c203708fe568cb60ea8102ca7f2d1d8b1c3b
Christopher Su <christophersu9@gmail.com>
2015-01-30 12:37:57 -0800

    custom output function

--- montecarlo/montecarlo.py
+++ montecarlo/montecarlo.py
@@ -1,22 +1,26 @@
 class montecarlo:
-    def __init__(self, func, setup=None, teardown=None):
+    def __init__(self, func, setup=None, teardown=None, output=None):
         self.func = func
         self.setup = setup
         self.teardown = teardown
+        if output:
+            self.output = output
+        else:
+            self.output = self.print_results
 
     @staticmethod
     def probability(success, iterations):
         return float(success)/iterations
 
     @staticmethod
-    def print_results(success, iterations, final=False):
+    def print_results(success=success, iteration=iterations, final=False):
         if final:
             print '======================'
             print '======= FINAL ========'
             print '======================'
         print 'Iteration #' + str(iterations) + ': ' + str(montecarlo.probability(success, iterations))
 
-    def run(self, iterations=1000000, print_every=10000):
+    def run(self, iterations=1000000, output_every=10000):
         g = {}
         if self.setup is not None:
             g = self.setup()
@@ -25,11 +29,11 @@ class montecarlo:
         for i in range(1, iterations+1):
             if self.func(g):
                 success += 1
-            if (i % print_every == 0):
-                self.print_results(success, i)
-        self.print_results(success, iterations, final=True)
+            if (i % output_every == 0):
+                self.output(success=success, iteration=i)
+        self.output(success=success, iteration=iterations, final=True)
 
         if self.teardown is not None:
             self.teardown()
 
-        return self.probability(success, iterations)
+        return self.output(success=success, iteration=iterations)
